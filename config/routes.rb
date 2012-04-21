16fc81d982ca5e9dbe1a59296b91b9106477dbc8
Andres Montano <amontano@virginia.edu>
2012-04-21 00:38:10 -0600

    updating to rails 3.0.x

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
-ActionController::Routing::Routes.draw do |map|
-
-  map.namespace(:proxy_engine) do |proxy_engine|
-    proxy_engine.connect 'proxy_engine/utils/proxy/', :controller => 'utils', :action => 'proxy'
+Rails.application.routes.draw do
+  namespace :proxy_engine do
+    match 'proxy_engine/utils/proxy/' => 'utils#proxy'
   end
-
 end
